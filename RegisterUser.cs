8345f1e6feb92237e732054b2d4461630220eceb
Fernando Kosciureski <131266294+FerLondon@users.noreply.github.com>
2025-01-31 07:37:40 -0300

    Update RegisterUser.cs

--- RegisterUser.cs
+++ RegisterUser.cs
@@ -8,10 +8,10 @@ namespace MySite.Models
 {
     public class RegisterUser
     {
-        [RemoteClientServer("CheckEmailIsValid", "User", ErrorMessage = "Usuário already taken")]
+        [RemoteClientServer("CheckEmailIsValid", "User", ErrorMessage = "Username is already in use")]
         [DisplayName("E-mail")]
-        [Required(ErrorMessage = "E-mail is invalid")]
-        [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$", ErrorMessage = "E-mail is invalid")]
+        [Required(ErrorMessage = "The email is invalid.")]
+        [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$", ErrorMessage = "The email is invalid.")]
         public string Email { get; set; }
 
     }
